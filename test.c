b7c9b5ad9ae6401734db24d40e61efc4b25adb59
enkheeboldu05 <61165406+derealgyoza@users.noreply.github.com>
2024-01-02 00:25:42 +0900

    failed test

--- test.c
+++ test.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
 int main(){
-    printf("hello world");
-    return 0;
+ int i;
+ int rows = 10;
+ for(i==0; i <= 0; i++){
+ for(int i = 0; i < rows; i++){
+		
+		for(int j = 0; j <= i; j++){
+			printf("# ");
+		}
+		printf("\n");
+		}
+	
+		
+	int test = 10;
+	
+	for(int i = 0; i < test; i++){
+		
+		for(int j = 0; j <=  test -i; j++ ){
+			printf("# ");
+		}
+		printf("\n");
+		}
+ }
+ 
+	return 0;
 }
